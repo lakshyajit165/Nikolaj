817e8190c3ff48129bc3aef5bf2923ec0b74f5ed
Saswat <saswatbe@gmail.com>
2019-10-30 10:53:17 +0530

    changed new chat service condition on ticket creation

--- bot-watson/src/main/java/com/stackroute/helpdesk/userRequest/service/ChatService.java
+++ bot-watson/src/main/java/com/stackroute/helpdesk/userRequest/service/ChatService.java
@@ -89,7 +89,8 @@ public class ChatService implements ChatServiceInterface {
 
             //calling generate ticket function(should be called only once and should not be greeting message")
             //if (!((responseIntents.size()!=0)&&(responseIntents.get(0).getIntent().equals("greetings")))){
-            if (!((responseIntents.size()!=0)&&(responseIntents.get(0).getIntent().equals("Greetings")))) {
+            if ((responseIntents.size()!=0)&&(!(responseIntents.get(0).getIntent().equals("Greetings")))) {
+                System.out.println("Inside ticket creation in chat service");
                 TicketModel ticketModel = new TicketModel();
                 ticketModel.setRaisedBy(userRequest.getEmailId());
                 ticketModel.setAssignedTo("bot");
