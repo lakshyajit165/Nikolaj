754dbf69db5a33a9c82189fdb0fbf77b047a9b41
maprana1 <maprana1@in.ibm.com>
2019-10-31 17:19:11 +0530

    command list refactored and environment variable changed

--- command-registry-angular/frontend/command-registry-ui/src/environments/environment.prod.ts
+++ command-registry-angular/frontend/command-registry-ui/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiGatewayUrl: 'http://15.206.36.205:8765/',
+  intentCommandMappingUrl: 'optimus/api/v1/intentName/commandName',
+  reportUrl: 'commandregistry/api/v1/commandregistry/reports/type?type=${type}',
+  commandUrl: 'commandregistry/api/v1/commandregistry/commands'
 };
