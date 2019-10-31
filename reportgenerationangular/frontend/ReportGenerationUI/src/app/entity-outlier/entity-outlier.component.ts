5055446bc72351a0510afbc9a8e364a1e4698ef3
garima-30 <anshugari07@gmail.com>
2019-11-01 02:04:40 +0530

    ui refactored

--- reportgenerationangular/frontend/ReportGenerationUI/src/app/entity-outlier/entity-outlier.component.ts
+++ reportgenerationangular/frontend/ReportGenerationUI/src/app/entity-outlier/entity-outlier.component.ts
@@ -12,32 +12,56 @@ import { Router } from '@angular/router';
 })
 export class EntityOutlierComponent implements OnInit {
 
-  constructor(public reportService: ReportService, private router: Router) {}
-
+  public maxDate : Date = new Date();
+  
   public serviceReport: IService[] = [];
-
+  
   public dataSource: IService[] = [];
-
+  
   public entity: string[] = new Array();
-
+  
   public isBarChart: boolean;
 
   public queriesRaised: number[] = new Array();
 
   public queriesResolved: number[] = new Array();
-
+  
   public leadTime: number[] = new Array();
-
+  
   startDate: string;
-
   endDate: string;
 
-  emptyData: boolean ;
+  emptyData : boolean ;
 
-  public scrollStatus = true;
-
+  public scrollStatus : boolean = true;
+  
   displayedColumns: string[] = ['entity', 'queriesRaised'];
 
+  ngOnInit(): void {
+
+
+    this.reportService.getServiceReport('', '').subscribe(data => {
+      this.emptyData = false;
+      if(data.result.length === 0)
+      {
+        this.emptyData = true;
+      }
+      this.serviceReport = data.result;
+      console.log("service report " + this.serviceReport);
+      this.serviceReport.map(element => {
+        this.entity.push(element.entity);
+        this.queriesRaised.push(element.queriesRaised);
+        this.queriesResolved.push(element.queriesResolved);
+        this.leadTime.push(element.leadTime);
+        this.dataSource = this.serviceReport;
+
+      });
+    })
+    
+  }
+
+  constructor(public reportService: ReportService, private router: Router) {}
+    
     public pieChartOptions: ChartOptions = {
       responsive: true,
       legend: {
@@ -58,32 +82,10 @@ export class EntityOutlierComponent implements OnInit {
     public pieChartLegend = true;
     public pieChartColors = [
       {
-        backgroundColor: [ '#B6BDF1', '#FFD5F6', '#37FACC', '#DAFCBB', '#FFFFC6', '#F77D97', '#4AA4F9'],
+        backgroundColor: [ "#B6BDF1", "#FFD5F6", "#37FACC", "#DAFCBB", "#FFFFC6", "#F77D97", "#4AA4F9"],
       },
     ];
-
-  ngOnInit(): void {
-
-
-    this.reportService.getServiceReport('', '').subscribe(data => {
-      this.emptyData = false;
-      if (data.result.length === 0) {
-        this.emptyData = true;
-      }
-      this.serviceReport = data.result;
-      console.log('service report ' + this.serviceReport);
-      this.serviceReport.map(element => {
-        this.entity.push(element.entity);
-        this.queriesRaised.push(element.queriesRaised);
-        this.queriesResolved.push(element.queriesResolved);
-        this.leadTime.push(element.leadTime);
-        this.dataSource = this.serviceReport;
-
-      });
-    });
-
-  }
-
+  
 
   // events
   public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
@@ -94,26 +96,27 @@ export class EntityOutlierComponent implements OnInit {
     console.log(event, active);
   }
 
-  // to convert the input from datepicker into the right format
+  //to convert the input from datepicker into the right format
   convert(date: Date): string {
     return date.toLocaleDateString('en-GB', {
-      year: 'numeric', month: 'numeric', day: 'numeric'
-    }).replace(/\//gi, '-').split('-').reverse().join('-');
+      year: 'numeric', month: 'numeric', day: 'numeric' 
+    }).replace(/\//gi, '-').split('-').reverse().join('-')
   }
 
-  // to empty the array of service report
+  //to empty the array of service report
   resetGraph() {
     this.entity.length = 0;
     this.queriesRaised.length = 0;
     this.queriesResolved.length = 0;
     this.leadTime.length = 0;
-    this.dataSource.length = 0;
-    this.dataSource.splice(0, this.dataSource.length);
+    this.dataSource.length=0;
+    this.dataSource.splice(0,this.dataSource.length)
     this.dataSource = [];
-    console.log('kitni aarhi h ' + this.dataSource.length);
+    console.log("kitni aarhi h " + this.dataSource.length)
   }
 
-  // when event is fired on selecting some particular date
+  //when event is fired on selecting some particular date
+
   getStartDate(value: Date) {
     console.log(value);
     this.resetGraph();
@@ -123,6 +126,7 @@ export class EntityOutlierComponent implements OnInit {
 
 
   getEndDate(value: Date) {
+    console.log('event dekho ->', typeof event);
     this.resetGraph();
     this.endDate = this.convert(value);
     this.sendDates();
@@ -135,7 +139,9 @@ export class EntityOutlierComponent implements OnInit {
     this.reportService.getServiceReport(this.startDate, this.endDate)
       .subscribe(data => {
         this.emptyData = false;
-        if (data.result.length === 0) {
+        if(data.result.length === 0)
+        {
+          console.log("inside if");
           this.emptyData = true;
         }
         this.serviceReport = data.result;
@@ -144,8 +150,8 @@ export class EntityOutlierComponent implements OnInit {
           this.queriesRaised.push(element.queriesRaised);
           this.queriesResolved.push(element.queriesResolved);
           this.leadTime.push(element.leadTime);
-          this.dataSource = this.serviceReport;
-          console.log(data.result);
+          this.dataSource=this.serviceReport;
+          console.log(data.result)
         });
       });
   }
@@ -154,7 +160,7 @@ export class EntityOutlierComponent implements OnInit {
   toggleChart() {
     this.isBarChart = !this.isBarChart;
     console.log('konsa chart->', this.isBarChart);
-
+  
 }
 
 }
