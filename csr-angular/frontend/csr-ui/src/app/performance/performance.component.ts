e7212dc5f17bc8faae6e4cc1c1bf29cb70240316
Lakshyajit <lakshyajit165@gmail.com>
2019-11-01 15:21:19 +0530

    Updated performance component

--- csr-angular/frontend/csr-ui/src/app/performance/performance.component.ts
+++ csr-angular/frontend/csr-ui/src/app/performance/performance.component.ts
@@ -22,8 +22,8 @@ export class PerformanceComponent implements OnInit {
   response2: object;
   responseTaken: object[];
   responseResolved: object[];
-  queryTakenCount: number[] = [];
-  queryResolvedCount: number[] = [];
+  queryTakenCount: object[] = [];
+  queryResolvedCount: object[] = [];
   dates: string[] = [];
   csrmail: string;
 
@@ -74,11 +74,16 @@ export class PerformanceComponent implements OnInit {
       this.responseTaken = this.response1[this.result];
       console.log(this.responseTaken);
 
-      this.responseTaken.forEach(ele => {
-        // console.log('element is ', element);
-        this.dates.push(moment(ele[this.timestamp]).format('DD-MM-YYYY'));
-        this.queryTakenCount.push(ele[this.total]);
-      });
+      this.dates = Object.keys(this.responseTaken);
+      this.queryTakenCount = Object.values(this.responseTaken);
+
+      // this.responseTaken.forEach(ele => {
+      //   // console.log('element is ', element);
+      //   this.dates.push(moment(ele[this.timestamp]).format('DD-MM-YYYY'));
+      //   this.queryTakenCount.push(ele[this.total]);
+      // });
+
+
       // this.responseResolved = this.responseTaken.map(ele => {
       //   this.dates.push(moment(ele[this.timestamp]).format('DD-MM-YYYY'));
       //   return ele[this.total];
@@ -87,8 +92,10 @@ export class PerformanceComponent implements OnInit {
         this.response2 = response;
         this.responseResolved = this.response2[this.result];
 
-        this.responseResolved.forEach(ele => {
-        this.queryResolvedCount.push(ele[this.total]);
+        this.queryResolvedCount = Object.values(this.responseResolved);
+
+        // this.responseResolved.forEach(ele => {
+        // this.queryResolvedCount.push(ele[this.total]);
       });
 
         // this.queryResolvedCount = this.responseResolved.map(ele => {
@@ -100,7 +107,7 @@ export class PerformanceComponent implements OnInit {
 
       // console.log(this.dates);
       // console.log(this.queryTakenCount);
-    });
+    
   }
 
 
