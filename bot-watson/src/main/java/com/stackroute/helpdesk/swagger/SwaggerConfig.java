d1363b72a5be712be06b8b1cf1d84321999595e6
garima-30 <anshugari07@gmail.com>
2019-11-02 04:43:32 +0530

    changes in the swagger

--- bot-watson/src/main/java/com/stackroute/helpdesk/swagger/SwaggerConfig.java
+++ bot-watson/src/main/java/com/stackroute/helpdesk/swagger/SwaggerConfig.java
@@ -14,6 +14,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
+@Profile("dev")
 public class SwaggerConfig {
     @Bean
     public Docket productApi() {
