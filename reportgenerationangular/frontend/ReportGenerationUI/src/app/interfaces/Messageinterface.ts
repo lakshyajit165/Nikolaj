15d12a0bd468e9cb1ade4e9873ae85a2896f28ce
garima-30 <anshugari07@gmail.com>
2019-11-02 04:35:29 +0530

    chnges in environment.ts

---
+++ reportgenerationangular/frontend/ReportGenerationUI/src/app/interfaces/Messageinterface.ts
@@ -0,0 +1,6 @@
+export class Message {
+    sender: string;
+    content: string;
+    type: string;
+    emailId: string;
+ }
--- reportgenerationangular/frontend/ReportGenerationUI/src/environments/environment.prod.ts
+++ reportgenerationangular/frontend/ReportGenerationUI/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  url: 'http://localhost:9003/',
   apigateway: 'https://nikolaj-dev.stackroute.io/'
 };
