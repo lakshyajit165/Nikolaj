35ef759a020393372efca980304151946bf30494
ayusmod1 <ayusmod1@in.ibm.com>
2019-11-04 04:09:47 +0530

    commnad registry angular updated

--- command-registry/src/main/java/com/stackroute/helpdesk/commandregistry/trackissueservice/service/NoIntentReport.java
+++ command-registry/src/main/java/com/stackroute/helpdesk/commandregistry/trackissueservice/service/NoIntentReport.java
@@ -29,71 +29,127 @@ public class NoIntentReport {
 
         reportDetails.stream().forEach(entities -> {
             if(isNullOrEmpty(entities.getEntity()))
-                setOfEntities.add("null");
+                setOfEntities.add("noentity");
             else
                 setOfEntities.add(entities.getEntity());
         });
 
         System.out.println("entities list = " + setOfEntities);
         setOfEntities.forEach(uniqueEntity -> {
-            HashMap<String, Object> entity = new HashMap<>();
-            HashMap<String, Object> mappedEntity = new HashMap<>();
-            List<HashMap<String, Object>> intentsList = new ArrayList<>();
-            List<HashMap<String, Object>> mappedIntentsList = new ArrayList<>();
-            HashSet<String> setOfIntents = new HashSet();
-            reportDetails.stream().forEach(entities -> {
-                if(uniqueEntity == entities.getEntity() || (uniqueEntity == "null")) {
+            if ((uniqueEntity == "noentity")) {
 
-                    entities.getIntentList().stream().forEach(intent -> {
-                        setOfIntents.add(intent.getIntent());
-                    });
-
-                    setOfIntents.forEach(uniqueIntent -> {
-                        HashMap<String, Object> intents = new HashMap<>();
-                        HashMap<String, Object> mappedIntents = new HashMap<>();
-                        List<HashMap<String, Object>> queriesList = new ArrayList<>();
-                        List<HashMap<String, Object>> mappedIntentQueriesList = new ArrayList<>();
-                        System.out.println("unique intent = " + uniqueIntent);
+                HashMap<String, Object> entity = new HashMap<>();
+                HashMap<String, Object> mappedEntity = new HashMap<>();
+                List<HashMap<String, Object>> intentsList = new ArrayList<>();
+                List<HashMap<String, Object>> mappedIntentsList = new ArrayList<>();
+                HashSet<String> setOfIntents = new HashSet();
+                reportDetails.stream().forEach(entities -> {
+                    if (uniqueEntity == entities.getEntity() || (uniqueEntity == "noentity")) {
                         entities.getIntentList().stream().forEach(intent -> {
-                            System.out.println("current intent = " + intent.getIntent() + " but unique intent = " + uniqueIntent);
-                            if (uniqueIntent == intent.getIntent()) {
-                                HashMap<String, Object> queries = new HashMap<>();
-                                System.out.println("intent name = " + intent.getIntent());
-                                if((intent.getIntent() == null)||(intent.getIntent().equalsIgnoreCase(""))||(intent.getIntent().equalsIgnoreCase("nointent"))) {
-                                    queries.put("name", intent.getTicketName());
-                                    queries.put("size", 300);
-                                    queriesList.add(queries);
-                                    System.out.println("queries = "+ queries.get("name"));
-                                }
-                                else {
-                                    queries.put("name", intent.getTicketName());
-                                    queries.put("size", 300);
-                                    mappedIntentQueriesList.add(queries);
-                                }
-                            }
-                            System.out.println("queries list = " + queriesList);
+                            setOfIntents.add(intent.getIntent());
                         });
-                        intents.put("children", queriesList);
-                        intents.put("name", uniqueIntent);
-                        mappedIntents.put("children", mappedIntentQueriesList);
-                        mappedIntents.put("name", uniqueIntent);
-                        intentsList.add(intents);
-                        mappedIntentsList.add(mappedIntents);
-                    });
-                }
-            });
-            entity.put("children", intentsList);
-            entity.put("name", uniqueEntity);
-            entitiesList.add(entity);
 
-            mappedEntity.put("children", mappedIntentsList);
-            mappedEntity.put("name", uniqueEntity);
-            mappedEntitiesList.add(mappedEntity);
+                        setOfIntents.forEach(uniqueIntent -> {
+                            HashMap<String, Object> intents = new HashMap<>();
+                            HashMap<String, Object> mappedIntents = new HashMap<>();
+                            List<HashMap<String, Object>> queriesList = new ArrayList<>();
+                            List<HashMap<String, Object>> mappedIntentQueriesList = new ArrayList<>();
+                            System.out.println("unique intent = " + uniqueIntent);
+                            entities.getIntentList().stream().forEach(intent -> {
+                                System.out.println("current intent = " + intent.getIntent() + " but unique intent = " + uniqueIntent);
+                                if (uniqueIntent == intent.getIntent()) {
+                                    HashMap<String, Object> queries = new HashMap<>();
+                                    System.out.println("intent name = " + intent.getIntent());
+                                    if ((intent.getIntent() == null) || (intent.getIntent().equalsIgnoreCase("")) || (intent.getIntent().equalsIgnoreCase("nointent"))) {
+                                        queries.put("name", intent.getTicketName());
+                                        queries.put("size", 300);
+                                        queriesList.add(queries);
+                                        System.out.println("queries = " + queries.get("name"));
+                                    } else {
+                                        queries.put("name", intent.getTicketName());
+                                        queries.put("size", 300);
+                                        mappedIntentQueriesList.add(queries);
+                                    }
+                                }
+                                System.out.println("queries list = " + queriesList);
+                            });
+                            intents.put("children", queriesList);
+                            intents.put("name", uniqueIntent);
+                            mappedIntents.put("children", mappedIntentQueriesList);
+                            mappedIntents.put("name", uniqueIntent);
+                            intentsList.add(intents);
+                            mappedIntentsList.add(mappedIntents);
+
+                        });
+                    }
+                });
+                entity.put("children", intentsList);
+                entity.put("name", uniqueEntity);
+                entitiesList.add(entity);
+
+                entity.put("children", mappedIntentsList);
+                entity.put("name", uniqueEntity);
+                entitiesList.add(mappedEntity);
+            }
+            else {
+                HashMap<String, Object> entity = new HashMap<>();
+                HashMap<String, Object> mappedEntity = new HashMap<>();
+                List<HashMap<String, Object>> intentsList = new ArrayList<>();
+                List<HashMap<String, Object>> mappedIntentsList = new ArrayList<>();
+                HashSet<String> setOfIntents = new HashSet();
+                reportDetails.stream().forEach(entities -> {
+                    if (uniqueEntity == entities.getEntity() || (uniqueEntity == "noentity")) {
+                        entities.getIntentList().stream().forEach(intent -> {
+                            setOfIntents.add(intent.getIntent());
+                        });
+
+                        setOfIntents.forEach(uniqueIntent -> {
+                            HashMap<String, Object> intents = new HashMap<>();
+                            HashMap<String, Object> mappedIntents = new HashMap<>();
+                            List<HashMap<String, Object>> queriesList = new ArrayList<>();
+                            List<HashMap<String, Object>> mappedIntentQueriesList = new ArrayList<>();
+                            System.out.println("unique intent = " + uniqueIntent);
+                            entities.getIntentList().stream().forEach(intent -> {
+                                System.out.println("current intent = " + intent.getIntent() + " but unique intent = " + uniqueIntent);
+                                if (uniqueIntent == intent.getIntent()) {
+                                    HashMap<String, Object> queries = new HashMap<>();
+                                    System.out.println("intent name = " + intent.getIntent());
+                                    if ((intent.getIntent() == null) || (intent.getIntent().equalsIgnoreCase("")) || (intent.getIntent().equalsIgnoreCase("nointent"))) {
+                                        queries.put("name", intent.getTicketName());
+                                        queries.put("size", 300);
+                                        queriesList.add(queries);
+                                        System.out.println("queries = " + queries.get("name"));
+                                    } else {
+                                        queries.put("name", intent.getTicketName());
+                                        queries.put("size", 300);
+                                        mappedIntentQueriesList.add(queries);
+                                    }
+                                }
+                                System.out.println("queries list = " + queriesList);
+                            });
+                            intents.put("children", queriesList);
+                            intents.put("name", uniqueIntent);
+                            mappedIntents.put("children", mappedIntentQueriesList);
+                            mappedIntents.put("name", uniqueIntent);
+                            intentsList.add(intents);
+                            mappedIntentsList.add(mappedIntents);
+
+                        });
+                    }
+                });
+                entity.put("children", intentsList);
+                entity.put("name", uniqueEntity);
+                mappedEntitiesList.add(entity);
+
+                entity.put("children", mappedIntentsList);
+                entity.put("name", uniqueEntity);
+                mappedEntitiesList.add(mappedEntity);
+            }
         });
         noCommand.put("children", entitiesList);
-        noCommand.put("name", "noIntent");
-        commandMapped.put("name", "intentMapped");
-        commandMapped.put("children",mappedEntitiesList);
+        noCommand.put("name", "entity mapped");
+        commandMapped.put("name", "entity mapped");
+        commandMapped.put("children", mappedEntitiesList);
         reportList.add(noCommand);
         reportList.add(commandMapped);
 
