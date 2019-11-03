34471d0b4fec2a1bed3f714a27eb0324c7710120
ayusmod1 <ayusmod1@in.ibm.com>
2019-11-03 17:22:41 +0530

    receiving data from messaging

--- socketserver/src/main/java/com/stackroute/helpdesk/sockets/service/SocketIdService.java
+++ socketserver/src/main/java/com/stackroute/helpdesk/sockets/service/SocketIdService.java
@@ -39,6 +39,16 @@ public class SocketIdService implements ISocketIdService{
 
 	public void removeSocket(String emailId){
 		System.out.println("emailId = " + emailId);
-		iSocketIdRepo.deleteById(emailId);
+		List<SocketStore> socketStore1 = (List<SocketStore>) iSocketIdRepo.findAll();
+		socketStore1.forEach(socketStore -> {
+			System.out.println("email id in socket store = " + socketStore.getEmailId());
+			System.out.println("socket object stored = " + socketStore);
+		});
+		Optional<SocketStore> optionalSocketStore = iSocketIdRepo.findById(emailId);
+		SocketStore socketStore = null;
+		if(optionalSocketStore.isPresent()) {
+			socketStore = optionalSocketStore.get();
+			iSocketIdRepo.delete(socketStore);
+		}
 	}
 }
