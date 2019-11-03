6598eea0d27c8afcf9bd5d325d07a605bd5d8f10
Lakshyajit <lakshyajit165@gmail.com>
2019-11-03 16:13:18 +0530

    changes in chat controller

--- bot-watson/src/main/java/com/stackroute/helpdesk/userRequest/controller/ChatController.java
+++ bot-watson/src/main/java/com/stackroute/helpdesk/userRequest/controller/ChatController.java
@@ -40,15 +40,14 @@ public class ChatController implements MessageListener {
                     !(botResult.contentEquals("Hi I am Optimus, how may I help you ")) &&
                     !(botResult.contentEquals("Hello how can I help you")) &&
                     !(botResult.contentEquals("Sure, how can I help you")) &&
-                    !(botResult.contentEquals("How much would you like to rate us")) &&
-                    !(botResult.contentEquals("star"))
+                    !(botResult.contentEquals("How much would you like to rate us"))
             ) {
                 Thread.sleep(1000);
                     publishReply(chatMessage.getEmailId(), chatMessage.getEmailId() + "_chat_messages", "Do you have any more queries");
 
                 System.out.println("publishing back from botwatson in channel = " + chatMessage.getEmailId() + "_chat_messages");
             }
-            
+
 
         }catch (IOException | InterruptedException ioException) {
             ioException.printStackTrace();
@@ -60,7 +59,7 @@ public class ChatController implements MessageListener {
         chatMessage.setContent(messageToSend);
         chatMessage.setEmailId(emailId);
         chatMessage.setSender("bot");
-        if (messageToSend.equals("star"))
+        if (messageToSend.equals("How much would you like to rate us"))
             chatMessage.setType("star");
         else
             chatMessage.setType("bot");
